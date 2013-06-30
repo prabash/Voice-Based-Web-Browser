d7525b6529c09e8315b7a47537655505ee9c116a
Prabash <prabash.darshana@gmail.com>
2013-06-30 13:54:47 +0530

    Modified the TabItemHeader Code Behind

--- UWIC.FinalProject.WebBrowser/Controller/TabItemHeader.xaml.cs
+++ UWIC.FinalProject.WebBrowser/Controller/TabItemHeader.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using UWIC.FinalProject.WebBrowser.ViewModel;
 
 namespace UWIC.FinalProject.WebBrowser.Controller
@@ -19,9 +8,9 @@ namespace UWIC.FinalProject.WebBrowser.Controller
     /// <summary>
     /// Interaction logic for TabItemHeader.xaml
     /// </summary>
-    public partial class TabItemHeader : UserControl
+    public partial class TabItemHeader
     {
-        private static TabItemViewModel _ViewModel { get; set; }
+        private static TabItemViewModel ViewModel { get; set; }
         public CharacterCasing Casing = CharacterCasing.Upper;
 
         public TabItemHeader(BitmapImage image, string title)
@@ -30,20 +19,19 @@ namespace UWIC.FinalProject.WebBrowser.Controller
             pageIcon.Source = image;
             PageTitle.Text = title;
             ToolTipService.SetToolTip(PageTitle, PageTitle.Text);
-            this.Resources.Add("ConvertUpperCase", Casing);
+            Resources.Add("ConvertUpperCase", Casing);
         }
 
         private void btnClose_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
-            var parent = (UIElement)this.Parent;
-            var _parent = (TabItem)parent;
-            int hashCode = _parent.GetHashCode();
-            _ViewModel.RemoveTabItem(hashCode);
+            var parent = (TabItem)(Parent);
+            var hashCode = parent.GetHashCode();
+            ViewModel.RemoveTabItem(hashCode);
         }
 
-        public static void setViewModel(TabItemViewModel vm)
+        public static void SetViewModel(TabItemViewModel vm)
         {
-            _ViewModel = vm;
+            ViewModel = vm;
         }
     }
 }
